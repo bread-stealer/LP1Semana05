ccaa55998940b42f8842def6a96cb624e4622b5b
bread-stealer <guilhermesilvanegrinho@gmail.com>
2026-03-19 15:06:35 +0000

    First commit

---
+++ LP1Semana05/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LP1Semana05
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
