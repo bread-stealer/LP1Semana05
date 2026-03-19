50555c967149102b54db9b34c9d224b186243c37
bread-stealer <guilhermesilvanegrinho@gmail.com>
2026-03-19 16:28:12 +0000

    New core-logic branch added to repository

--- DungeonStats/Program.cs
+++ DungeonStats/Program.cs
@@ -4,9 +4,16 @@ namespace DungeonStats
 {
     public class Program
     {
+        private static int Damage(int attack)
+            => attack;
+        private static int Damage(int attack, int defense)
+            => Math.Max(attack - defense, 0);
+
+        private static int CriticalHit(int damage)
+            => damage <= 0 ? 0 : 1 + CriticalHit(damage - 1);
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            
         }
     }
 }
